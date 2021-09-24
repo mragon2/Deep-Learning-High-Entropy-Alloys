2b6ca55ed3bac597e373a72e878de32bbb86a4f3
Marco Ragone <mragon2@uic.edu>
2021-09-24 17:30:18 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+'''
+Author: Marco Ragone, Computational Multiphase Transport Laboratory, University of Illinois at Chicago
+'''
+
+import os
+
+if __name__ == '__main__':
+
+	os.system('python scripts/make_HEA_data.py')
+	#os.system('python scripts/make_HEA_data_multiprocessing.py')
+
+
+
